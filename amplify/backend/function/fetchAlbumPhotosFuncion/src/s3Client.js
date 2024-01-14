ed3c3cb31cb2f47764fe44a0f804aa01e1602421
zqy <tony.zhou2023@gmail.com>
2024-01-14 17:35:17 +0800

    changed region to us-west-2

--- amplify/backend/function/fetchAlbumPhotosFuncion/src/s3Client.js
+++ amplify/backend/function/fetchAlbumPhotosFuncion/src/s3Client.js
@@ -1,6 +1,6 @@
 const { S3Client } = require("@aws-sdk/client-s3");
-const REGION = "ap-east-1";
+const REGION = "us-west-2";
 const s3Client = new S3Client({ region: REGION });
 
 exports.s3Client = s3Client;
-exports.bucketName = "luffysfightclub-hongkong";
+exports.bucketName = "luffysfightclub";
